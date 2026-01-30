b6fe13cf7472d11b0ef905c6c13fce6bdb8aeda7
Matt Allen <matthew.allen@guidantmeasurement.com>
2026-01-30 15:04:07 -0500

    Switch to setuptools-scm for versioning.  Updated smoke test to just use PCAP for now.

--- smoke_test_EXE.sh
+++ smoke_test_EXE.sh
@@ -6,7 +6,7 @@
  ./dist/modbus-sniffer.exe watch --pcap "tests/pcaps/sample.pcapng" --fc 3 --watch 100 --deltas-only
  ./dist/modbus-sniffer.exe watch --pcap "tests/pcaps/sample.pcapng" --fc 3 --trigger-change-reg 100 --include-regs 200 205 --payload-format json
 
- echo -n "Tests using 'Ethernet 4' interface\n"
- ./dist/modbus-sniffer.exe watch --iface "Ethernet 4" --watch 100 200 205
- ./dist/modbus-sniffer.exe watch --iface "Ethernet 4" --fc 3 --watch 100 --deltas-only
- ./dist/modbus-sniffer.exe watch --iface "Ethernet 4" --fc 3 --trigger-change-reg 100 --include-regs 200 205 --payload-format json
+# echo -n "Tests using 'Ethernet 4' interface\n"
+# ./dist/modbus-sniffer.exe watch --iface "Ethernet 4" --watch 100 200 205
+# ./dist/modbus-sniffer.exe watch --iface "Ethernet 4" --fc 3 --watch 100 --deltas-only
+# ./dist/modbus-sniffer.exe watch --iface "Ethernet 4" --fc 3 --trigger-change-reg 100 --include-regs 200 205 --payload-format json
